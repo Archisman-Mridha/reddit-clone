794f7909be10261a251188faf92099814b44e77d
Archisman <archisman@obmondo.com>
2025-02-03 03:13:01 +0530

    💨 chore : a fresh start
    Signed-off-by: Archisman <archisman@obmondo.com>

--- apps/backend/src/modules/authentication/get-current-user-id.decorater.ts
+++
@@ -1,9 +0,0 @@
-import { ExecutionContext, createParamDecorator } from "@nestjs/common"
-import { GqlExecutionContext } from "@nestjs/graphql"
-
-export const GetCurrentUserId = createParamDecorator(
-  (_: any, context: ExecutionContext): number => {
-    const userId = GqlExecutionContext.create(context).getContext().req.user
-    return parseInt(userId)
-  }
-)
--- apps/backend/src/modules/authentication/types.ts
+++
@@ -1,3 +0,0 @@
-export class JWTPayload {
-  userId: string
-}
